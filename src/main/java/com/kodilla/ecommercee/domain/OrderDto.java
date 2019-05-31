0025dcff7d6ebbdd98bc48968cfdd3332c0c572f
aradzikowska12 <aradzikowska12@gmail.com>
2019-05-31 16:03:00 +0200

    task JDP190502-4

---
+++ src/main/java/com/kodilla/ecommercee/domain/OrderDto.java
@@ -0,0 +1,42 @@
+package com.kodilla.ecommercee.domain;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+public class OrderDto {
+    private Long Id;
+    private LocalDate created;
+    private boolean paid;
+    private boolean realised;
+    private List<ProductDto> products;
+
+    public OrderDto(Long id, int yearOfOrder,int monthOfOrder,int dayOfOrder, boolean paid, boolean realised, List<ProductDto> products) {
+        Id = id;
+        this.created = LocalDate.of(yearOfOrder,monthOfOrder,dayOfOrder);
+        this.paid = paid;
+        this.realised = realised;
+        this.products = products;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public LocalDate getCreated() {
+        return created;
+    }
+
+    public boolean isPaid() {
+        return paid;
+    }
+
+    public boolean isRealised() {
+        return realised;
+    }
+
+    public List<ProductDto> getProducts() {
+        return products;
+    }
+}
