3d002e0ffb3e10d6a92889d7a6dd515d8096bfb7
Luke1024 <chajdas.lukasz@gmail.com>
2019-06-02 15:19:25 +0200

    task JDP190502-31

---
+++ src/main/java/com/kodilla/ecommercee/domain/CartProductAdderDto.java
@@ -0,0 +1,21 @@
+package com.kodilla.ecommercee.domain;
+
+import java.util.List;
+
+public class CartProductAdderDto {
+    private Long cartId;
+    private List<Product> products;
+
+    public CartProductAdderDto(Long cartId, List<Product> products) {
+        this.cartId = cartId;
+        this.products = products;
+    }
+
+    public Long getCartId() {
+        return cartId;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+}
---
+++ src/main/java/com/kodilla/ecommercee/domain/CartProductDeleterDto.java
@@ -0,0 +1,19 @@
+package com.kodilla.ecommercee.domain;
+
+public class CartProductDeleterDto {
+    private Long cartId;
+    private Long productId;
+
+    public CartProductDeleterDto(Long cartId, Long productId) {
+        this.cartId = cartId;
+        this.productId = productId;
+    }
+
+    public Long getCartId() {
+        return cartId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+}
