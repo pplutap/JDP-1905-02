573d525ee08f5b99f843e3e194d6ac40af0a55e8
aradzikowska12 <aradzikowska12@gmail.com>
2019-06-09 12:05:39 +0200

    konstruktor z listą produktów taskJDP190502-14

--- src/main/java/com/kodilla/ecommercee/domain/CartDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/CartDto.java
@@ -1,15 +1,17 @@
 package com.kodilla.ecommercee.domain;
 
+import java.util.List;
+
 public class CartDto {
     private Long cartId;
     private String groupId;
+    private List<Product> products;
 
     public CartDto() {
     }
 
-    public CartDto(Long cartId,String groupId) {
-        this.cartId = cartId;
-        this.groupId = groupId;
+    public CartDto(List<Product> products) {
+        this.products = products;
     }
 
     public Long getCartId() {
