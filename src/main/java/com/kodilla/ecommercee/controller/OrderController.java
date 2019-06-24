da397091f47b440f10971176d82c96b9fa53c7e1
Luke1024 <chajdas.lukasz@gmail.com>
2019-06-24 10:44:16 +0200

    update3

--- src/main/java/com/kodilla/ecommercee/controller/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/controller/OrderController.java
@@ -3,9 +3,7 @@ package com.kodilla.ecommercee.controller;
 import com.kodilla.ecommercee.domain.OrderCreationDto;
 import com.kodilla.ecommercee.domain.OrderDto;
 import com.kodilla.ecommercee.mapper.OrderMapper;
-import com.kodilla.ecommercee.service.CartService;
 import com.kodilla.ecommercee.service.OrderService;
-import com.kodilla.ecommercee.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,12 +21,6 @@ public class OrderController {
 
     @Autowired
     private OrderService orderService;
-
-    @Autowired
-    private CartService cartService;
-
-    @Autowired
-    private UserService userService;
 
     @RequestMapping(method = RequestMethod.GET, value = "getOrders")
     public List<OrderDto> getOrders() {
--- src/test/java/com/kodilla/ecommercee/controller/ProductControllerTest.java
+++ src/test/java/com/kodilla/ecommercee/controller/ProductControllerTest.java
@@ -82,10 +82,4 @@ public class ProductControllerTest {
                 .content(jsonContent))
                 .andExpect(status().isOk());
     }
-
-    @Test
-    public void deleteProduct() throws Exception{
-
-    }
-
 }
